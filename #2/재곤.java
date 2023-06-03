bb6e42250e53f274ad93a3d8b5b8c79c8d00012d
Seo Jaegon <jgoneit@gmail.com>
2023-06-03 13:30:18 +0900

    회의실 배정

--- #2/재곤.java
+++ #2/재곤.java
@@ -1,52 +1,36 @@
-import java.util.*;
-
-class Meeting implements Comparable<Meeting> {
-    int start;
-    int end;
-
-    public Meeting(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    /*
-    * 각 회의를 종료 시간 기준으로 정렬
-    * 종료시간이 같으면 시작 시간이 더 빠른 회의의 순서를 앞에 둠
-    */
- 
-    @Override
-    public int compareTo(Meeting o) {
-        if(this.end == o.end){
-            return this.start - o.start;
-        }
-        return this.end - o.end;
-    }
-}
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        int count = sc.nextInt();
-        List<Meeting> meetingList = new ArrayList<>();
-
-        for (int i = 0; i < count; i++) {
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-            meetingList.add(new Meeting(start, end));
+        System.out.print("회의의 수를 입력해 주세요: ");
+        int n = sc.nextInt();
+        int[][] arr = new int[n][2];
+        for(int i = 0; i < n; i++) {
+            System.out.println("시작 시간과 종료 시간을 입력해 주세요(index: " + (i+1) + ")");
+            // 회의 시작 시간
+            arr[i][0] = sc.nextInt();
+            // 회의 종료 시간
+            arr[i][1] = sc.nextInt();
         }
-
-        Collections.sort(meetingList);
-
-        int answer = 1;
-        int end = meetingList.remove(0).end;
-
-        for(Meeting meeting : meetingList){
-            if(meeting.start>=end){
-                answer++;
-                end = meeting.end;
+        sc.close();
+        Arrays.sort(arr, (o1, o2) -> {
+            if(o1[1] == o2[1]) {
+                return Integer.compare(o1[0], o2[0]);
+            } else {
+                return Integer.compare(o1[1], o2[1]);
+            }
+        });
+        int meetingCount = 0;
+        int endTime = -1;
+        for(int i = 0; i < n; i++) {
+            if(arr[i][0] > endTime) {
+                endTime = arr[i][1];
+                meetingCount++;
             }
         }
-
-        System.out.println(answer);
+        System.out.println(meetingCount);
     }
 }
